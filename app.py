b1c579552cd40754578594d9d9becdf55f83428d
Graeme Tank <graeme.tank@gmail.com>
2022-08-04 18:24:00 -0400

    setup database and Flask

--- app.py
+++ app.py
@@ -1,8 +1,33 @@
 # The Flask Weather App
 
+import datetime as dt
+import numpy as np
+import pandas as pd
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+# set up SQLite database
+engine = create_engine("sqlite:///hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+# save references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# create the session link
+session = Session(engine)
+
+# define Flask app
+app = Flask(__name__)
 
 
-# from flask import Flask
+
 
 # # create new flask instance
 # app = Flask(__name__)
